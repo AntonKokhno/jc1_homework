bbc449e8cbaeb378f0f09c28db9d56fe127f0c93
AntonKokhno <antonkokhno1392@gmail.com>
2022-03-14 22:30:48 +0300

    Homework1

---
+++ src/Task17/Task17.java
@@ -0,0 +1,32 @@
+package Task17;
+
+public class Task17 {
+    public static void main(String[] args) {
+        int i=12132132;
+        int j=1000;
+        int h =0;
+        int l=1;
+        int t=i/1000;
+        int k=i%1000;
+        int f=t/1000;
+        int o=t%1000;
+        int e=f/1000;
+
+        int d=f%1000;
+        while ( (k) > 0) {
+            l++;
+            h=l%3;
+
+        }
+        System.out.println(l+" "+h);
+        }
+
+
+
+
+
+
+
+
+    }
+
